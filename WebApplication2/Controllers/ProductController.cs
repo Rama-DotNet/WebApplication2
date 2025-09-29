b7f9909e5ae2e3897ddb7f88b20f101fe199fa6e
Ramasubbamma <Ramasubbamma@DESKTOP-VC8L64J>
2025-09-29 15:26:05 +0530

    I added the GET API in the product controller

---
+++ WebApplication2/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Model;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            // Placeholder response – no values assigned
+            return Ok(new List<string>()); // You can replace string with Product later
+        }
+
+        // GET: api/product/{id}
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            // Placeholder response – no actual product lookup
+            return Ok($"Requested product ID: {id}");
+        }
+    }
+}
+
