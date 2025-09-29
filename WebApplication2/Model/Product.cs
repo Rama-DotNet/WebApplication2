d034aceeb5ff2b7b0566ac19d5b1ce8f834e1131
Ramasubbamma <Ramasubbamma@DESKTOP-VC8L64J>
2025-09-29 14:36:29 +0530

    Added Product Model

---
+++ WebApplication2/Model/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Model
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductCategory { get; set; }
+        public int ProductCategoryId { get; set; }
+        public int Price { get; set; }
+    }
+}
