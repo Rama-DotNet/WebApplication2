c1fc7903a9f491d80ae1c828933e76b8731e8163
Ramasubbamma <Ramasubbamma@DESKTOP-VC8L64J>
2025-09-29 15:13:46 +0530

    Created get api in product controller

---
+++ WebApplication2/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Model;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            
+            return Ok(new List<string>());
+        }
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return Ok($"Requested product ID: {id}");
+        }
+    }
+}
+    
